9bd72dec67c3c56c8f0f590beb093e00eb4accc9
Mehul Kapadia <mehul.kaps@icloud.com>
2025-06-13 08:33:55 -0400

    Testing pre-commit 1

---
+++ src/main/java/com/example/precommit/hookpoc/PrecommitHookPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.precommit.hookpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecommitHookPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecommitHookPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/precommit/precommit_hook_poc/PrecommitHookPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.precommit.precommit_hook_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecommitHookPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
